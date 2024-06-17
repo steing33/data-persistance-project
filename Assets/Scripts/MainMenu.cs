1f85c1cf70ebd06c95f0656932ccc6e5f4fc7fa5
steing33 <155857577+steing33@users.noreply.github.com>
2024-06-17 09:31:49 -0700

    main menu
    - functional start/exit buttons

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartApp() {
+        Debug.Log("loading main scene...");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("exiting application...");
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
